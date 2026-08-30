534c2119e391f12d0168f694ef4bcba7a91df9ad
Josias Silva <josilva1233@gmail.com>
2026-08-30 19:55:49 -0300

    13312312

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,10 +23,10 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        $faker = \Faker\Factory::create(); // 🔥 ALTERNATIVA
+        // 🔥 Usar fake() helper do Laravel
         return [
-            'name' => $faker->name(),
-            'email' => $faker->unique()->safeEmail(),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
