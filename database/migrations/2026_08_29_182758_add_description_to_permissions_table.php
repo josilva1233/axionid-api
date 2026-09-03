89f8e8535622e1db9a990d86ed80e1c4620ae970
Josias Silva <josilva1233@gmail.com>
2026-09-03 14:24:10 -0300

    23432423

--- database/migrations/2026_08_29_182758_add_description_to_permissions_table.php
+++ database/migrations/2026_08_29_182758_add_description_to_permissions_table.php
@@ -6,14 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    public function up(): void
+    public function up()
     {
-        Schema::table('permissions', function (Blueprint $table) {
-            $table->text('description')->nullable()->after('label');
-        });
+        // Verifica se a coluna já existe
+        if (!Schema::hasColumn('permissions', 'description')) {
+            Schema::table('permissions', function (Blueprint $table) {
+                $table->text('description')->nullable()->after('label');
+            });
+        }
     }
 
-    public function down(): void
+    public function down()
     {
         Schema::table('permissions', function (Blueprint $table) {
             $table->dropColumn('description');
